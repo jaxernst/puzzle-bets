edac692d86fc8b19cb7cb71730bafcd3439c3956
jackson <jaxernst@gmail.com>
2024-01-03 23:19:02 -0800

    Break out active games page

--- packages/client/src/routes/+layout.svelte
+++ packages/client/src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script>
+  import { userGames } from "$lib/mud/gameStores";
   import { page } from "$app/stores";
   import WalletConnector from "$lib/components/WalletConnector.svelte";
+  import ActiveGames from "./ActiveGames.svelte";
   import ConnectUser from "./ConnectUser.svelte";
   import Header from "./Header.svelte";
   import "./styles.css";
@@ -38,19 +40,7 @@
     <section
       class="flex flex-col gap-2 bg-gray-600 rounded-2xl p-4 shadow-inner"
     >
-      <div class="font-mono text-gray-100">Active Games</div>
-      <div class="flex gap-2 items-center">
-        {#each [{ name: "jaxer.eth", game: "Wordle" }] as { name, game }}
-          <div
-            class="flex items-center gap-2 px-3 py-2 self-start rounded-lg text-white bg-lime-500 font-semibold text-center transition-all"
-          >
-            {game}
-            <span>|</span>
-            {name}
-            <span class="font-bold text-lg"> $1.21</span>
-          </div>
-        {/each}
-      </div>
+      <ActiveGames />
     </section>
 
     <section class="flex-grow bg-gray-600 p-4 rounded-2xl shadow-inner">
---
+++ packages/client/src/routes/ActiveGames.svelte
@@ -0,0 +1,26 @@
+<script lagn="ts">
+  import { userGames } from "$lib/mud/gameStores";
+  import { mud, user } from "$lib/mud/mudStore";
+  import {
+    Has,
+    HasValue,
+    getComponentValueStrict,
+    runQuery,
+  } from "@latticexyz/recs";
+
+  $: console.log(userGames);
+</script>
+
+<div class="font-mono text-gray-100">Active Games</div>
+<div class="flex gap-2 items-center">
+  {#each [{ name: "jaxer.eth", game: "Wordle" }] as { name, game }}
+    <div
+      class="flex items-center gap-2 px-3 py-2 self-start rounded-lg text-white bg-lime-500 font-semibold text-center transition-all"
+    >
+      {game}
+      <span>|</span>
+      {name}
+      <span class="font-bold text-lg"> $1.21</span>
+    </div>
+  {/each}
+</div>

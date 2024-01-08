d3c53fed360c32315545fa618669b85b8ac8e51f
jackson <jaxernst@gmail.com>
2024-01-08 12:57:02 -0700

    Styling + welcome

--- packages/client/src/routes/welcome/+page.svelte
+++ packages/client/src/routes/welcome/+page.svelte
@@ -1,11 +1,14 @@
 <script>
+  import { goto } from "$app/navigation";
   import puzzly from "$lib/assets/puzzly.png";
   import { promptConnectWallet } from "$lib/components/WalletConnector.svelte";
-  import { mud } from "$lib/mud/mudStore";
+  import { userWallet } from "$lib/mud/connectWallet";
+  import { mud, user } from "$lib/mud/mudStore";
 
   const loginAndConnect = async () => {
     const wallet = await promptConnectWallet();
-    mud.setup(wallet);
+    await mud.setup(wallet);
+    goto("/games/wordle");
   };
 </script>
 
@@ -16,7 +19,7 @@
     </div>
     <div class="text-center text-2xl font-bold">Welcome to Puzzle Bets.</div>
   </div>
-  <div class="flex px-6">
+  <div class="flex justify-center px-6">
     <div class="flex flex-col font-semibold">
       <div class="flex gap-3 items-center">
         <div class="w-3 h-[.3rem] rounded-full bg-lime-500"></div>
@@ -32,12 +35,20 @@
       </div>
     </div>
   </div>
-  <div class="flex-grow flex flex-col items-center h-full justify-end">
-    <button
-      on:click={loginAndConnect}
-      class="p-3 font-semibold border-2 border-lime-500 hover:bg-lime-500 hover:shadow transition-all duration-300 rounded-xl text-white"
-    >
-      Connect your wallet and choose a puzzle to begin.
-    </button>
+  <div class="flex flex-col items-center flex-grow justify-end p-6">
+    {#if !$user}
+      <button
+        on:click={loginAndConnect}
+        class="p-3 font-semibold border-2 border-lime-500 hover:bg-lime-500 hover:shadow transition-all duration-300 rounded-xl text-white"
+      >
+        Connect to Play
+      </button>
+    {:else}
+      <div
+        class="p-3 font-semibold border-2 border-lime-500 hover:bg-lime-500 hover:shadow transition-all duration-300 rounded-xl text-white"
+      >
+        Choose a game to play
+      </div>
+    {/if}
   </div>
 </div>

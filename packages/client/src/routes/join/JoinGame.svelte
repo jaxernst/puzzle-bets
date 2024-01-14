7db48b47b5c5121cfaff2f478893950647b760cb
jackson <jaxernst@gmail.com>
2024-01-14 14:50:14 -0800

    IMprove joining ux

--- packages/client/src/routes/join/JoinGame.svelte
+++ packages/client/src/routes/join/JoinGame.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
   import { page } from "$app/stores";
+  import DotLoader from "$lib/components/DotLoader.svelte";
   import Modal from "$lib/components/Modal.svelte";
   import { mud } from "$lib/mud/mudStore";
   import { gameNumberToType, GameStatus, type GameType } from "$lib/types";
   import { capitalized, urlGameIdToEntity } from "$lib/util";
   import { getComponentValueStrict, type Entity } from "@latticexyz/recs";
   import { createEventDispatcher } from "svelte";
+  import { slide } from "svelte/transition";
 
   export let gameId: Entity;
 
@@ -21,6 +23,17 @@
     GameStatus.Pending;
 
   const dispatch = createEventDispatcher();
+
+  let joinGameLoading = false;
+  const joinGame = async () => {
+    joinGameLoading = true;
+    try {
+      await $mud.systemCalls.joinGame(gameId);
+      dispatch("joined");
+    } finally {
+      joinGameLoading = false;
+    }
+  };
 </script>
 
 <div class="flex flex-col gap-2 max-w-[450px]">
@@ -31,13 +44,17 @@
     )}
   </div>
   <div class="text-sm text-gray-100"></div>
-  <div class="px-4">
+  <div class="px-4 flex justify-center">
     <button
-      class="bg-lime-500 text-white rounded-lg p-2"
-      on:click={() =>
-        $mud.systemCalls.joinGame(gameId).then(() => dispatch("joined"))}
+      in:slide={{ axis: "x" }}
+      class="bg-lime-500 text-white rounded-lg p-2 whitespace-nowrap hover:bg-lime-400 hover:shadow transition-all"
+      on:click={joinGame}
     >
-      Join to Reveal Puzzle
+      {#if joinGameLoading}
+        <DotLoader />
+      {:else}
+        Join to Reveal Puzzle
+      {/if}
     </button>
   </div>
 </div>

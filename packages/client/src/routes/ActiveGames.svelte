b72f466fda3e06d90ebaaf7ea1325856c9b6b8f7
jackson <jaxernst@gmail.com>
2024-01-14 14:51:13 -0800

    Show correct status

--- packages/client/src/routes/ActiveGames.svelte
+++ packages/client/src/routes/ActiveGames.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-  import { goto } from "$app/navigation";
   import { page } from "$app/stores";
   import { userGames } from "$lib/gameStores";
   import { getComponentValueStrict, type Entity } from "@latticexyz/recs";
-  import { type GameType } from "$lib/types";
+  import { type GameType, GameStatus } from "$lib/types";
+
   import { capitalized, shortenAddress } from "$lib/util";
   import { mud } from "$lib/mud/mudStore";
   import { user } from "$lib/mud/mudStore";
@@ -18,7 +18,7 @@
 
   let ethPrice = 2400;
 
-  const formatAsDollar = (value) => {
+  const formatAsDollar = (value: number) => {
     return new Intl.NumberFormat("en-US", {
       style: "currency",
       currency: "USD",
@@ -44,12 +44,12 @@
   <span class="text-lime-400">{activeGames.length}</span> Active Games
 </div>
 {#if activeGames.length}
-  <div class="flex flex-wrap gap-2 items-center">
-    {#each activeGames as { type, p1, p2, id, opponent }}
+  <div class="flex overflow-auto gap-1 no-scrollbar items-center">
+    {#each activeGames as { type, p1, p2, id, opponent, status }}
       {@const active = $page.params.gameId === parseInt(id, 16).toString()}
       <a
-        class={`flex flex-col px-3 py-2 self-start rounded-lg text-white font-semibold font-mono  transition-all
-          ${!active ? "" : "bg-lime-500"}
+        class={`flex flex-col p-2 self-start rounded-lg text-white font-semibold font-mono  transition-all
+          ${!active ? "bg-gray-500" : "bg-lime-500"}
         `}
         href={gameRoute(id, type)}
       >
@@ -62,10 +62,16 @@
           >
         </div>
         <div
-          class={`px-1 text-[.7em] italic 
-          ${active ? "text-lime-600 " : "text-gray-200"}`}
+          class={`px-1 text-[.7em] italic whitespace-nowrap 
+          ${active ? "text-lime-600 font-bold" : "text-gray-200"}`}
         >
-          waiting for {shortenAddress(opponent)}
+          {#if status === GameStatus.Pending}
+            waiting for opponent...
+          {:else if status === GameStatus.Active}
+            ready to solve...
+          {:else if status === GameStatus.Complete}
+            Puzzle complete
+          {/if}
         </div>
       </a>
     {/each}

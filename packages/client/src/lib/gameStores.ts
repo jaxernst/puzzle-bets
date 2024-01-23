781f1b615a7b71dc3cdef8fdf3a8f25526c56cee
jackson <jaxernst@gmail.com>
2024-01-22 20:45:28 -0800

    Factor out function

--- packages/client/src/lib/gameStores.ts
+++ packages/client/src/lib/gameStores.ts
@@ -1,4 +1,4 @@
-import { derived, writable } from "svelte/store";
+import { derived, get, writable } from "svelte/store";
 
 import { mud, user } from "./mud/mudStore";
 import {
@@ -11,6 +11,7 @@ import {
 } from "@latticexyz/recs";
 import { GameStatus, gameNumberToType, type Game } from "$lib/types";
 import { encodeEntity } from "@latticexyz/store-sync/recs";
+import type { SetupNetworkResult } from "./mud/setupNetwork";
 
 export const userGames = derived([mud, user], ([$mud, $user]) => {
   if (!$mud || !$mud.ready || !$user) return [];
@@ -26,51 +27,65 @@ export const userGames = derived([mud, user], ([$mud, $user]) => {
   ]);
 
   return Array.from([...p1Games, ...p2Games]).map((gameId) => {
-    const gameType =
-      gameNumberToType[
-        getComponentValueStrict($mud.components.GameType, gameId).value
-      ];
-
-    const p1 = getComponentValueStrict($mud.components.Player1, gameId).value;
-    const p2 = getComponentValue($mud.components.Player2, gameId)?.value;
-    const status = getComponentValueStrict($mud.components.GameStatus, gameId)
-      .value as GameStatus;
-
-    const betAmount =
-      getComponentValue(
-        $mud.components.Deposit,
-        encodeEntity(
-          { gameId: "bytes32", player: "address" },
-          { gameId: gameId as `0x${string}`, player: $user }
-        )
-      )?.value ?? 0n;
-
-    const startTime = getComponentValue(
-      $mud.components.GameStartTime,
-      gameId
-    )?.value;
-
-    const submissionWindow = getComponentValueStrict(
-      $mud.components.SubmissionWindow,
-      gameId
-    ).value;
-
-    const inviteExpiration = getComponentValueStrict(
-      $mud.components.InviteExpiration,
-      gameId
-    ).value;
-
+    const game = gameIdToGame(gameId, $mud.components);
     return {
-      id: gameId,
-      type: gameType,
-      status,
-      opponent: $user === p2 ? p1 : p2,
-      betAmount,
-      p1,
-      p2,
-      startTime,
-      submissionWindow,
-      inviteExpiration,
+      ...game,
+      opponent: $user === game.p1 ? game.p2 : game.p1,
     };
   });
 });
+
+export const getGame = derived(mud, ($mud) => {
+  return (gameId: Entity) => gameIdToGame(gameId, $mud.components);
+});
+
+const gameIdToGame = (
+  gameId: Entity,
+  mudComponents: SetupNetworkResult["components"]
+) => {
+  const gameType =
+    gameNumberToType[
+      getComponentValueStrict(mudComponents.GameType, gameId).value
+    ];
+
+  const p1 = getComponentValueStrict(mudComponents.Player1, gameId).value;
+  const p2 = getComponentValue(mudComponents.Player2, gameId)?.value;
+  const status = getComponentValueStrict(mudComponents.GameStatus, gameId)
+    .value as GameStatus;
+
+  const betAmount =
+    getComponentValue(
+      mudComponents.Deposit,
+      encodeEntity(
+        { gameId: "bytes32", player: "address" },
+        { gameId: gameId as `0x${string}`, player: p1 as `0x${string}` }
+      )
+    )?.value ?? 0n;
+
+  const startTime = getComponentValue(
+    mudComponents.GameStartTime,
+    gameId
+  )?.value;
+
+  const submissionWindow = getComponentValueStrict(
+    mudComponents.SubmissionWindow,
+    gameId
+  ).value;
+
+  const inviteExpiration = getComponentValueStrict(
+    mudComponents.InviteExpiration,
+    gameId
+  ).value;
+
+  return {
+    id: gameId,
+    type: gameType,
+    status,
+    betAmount,
+    p1,
+    p2,
+    startTime,
+    submissionWindow,
+    inviteExpiration,
+  };
+};

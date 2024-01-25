3fa1252035d25a5e6c19db382c0f935ce31b0a86
jackson <jaxernst@gmail.com>
2024-01-24 22:46:51 -0800

    Add custom color

--- packages/client/tailwind.config.js
+++ packages/client/tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "off-black": "#3e3e3e",
+      },
+    },
   },
   plugins: [],
 };

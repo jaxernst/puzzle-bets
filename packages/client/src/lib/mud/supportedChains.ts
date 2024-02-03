ea3727978f2904077b807dc65ce624da53241ce2
jackson <jaxernst@gmail.com>
2024-02-02 22:58:08 -0800

    override testnet rpc

--- packages/client/src/lib/mud/supportedChains.ts
+++ packages/client/src/lib/mud/supportedChains.ts
@@ -10,10 +10,35 @@
  *
  */
 
-import { type MUDChain, latticeTestnet, mudFoundry } from '@latticexyz/common/chains'
+import { type MUDChain, mudFoundry } from "@latticexyz/common/chains";
+
+// Override default lattice chain with 'miner.testnet' (from follower.testnet)
+const latticeTestnet = {
+  name: "Lattice Testnet",
+  id: 4242,
+  network: "lattice-testnet",
+  nativeCurrency: { decimals: 18, name: "Ether", symbol: "ETH" },
+  rpcUrls: {
+    default: {
+      http: ["https://miner.testnet-chain.linfra.xyz"],
+      webSocket: ["wss://miner.testnet-chain.linfra.xyz"],
+    },
+    public: {
+      http: ["https://miner.testnet-chain.linfra.xyz"],
+      webSocket: ["wss://miner.testnet-chain.linfra.xyz"],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: "Otterscan",
+      url: "https://explorer.testnet-chain.linfra.xyz",
+    },
+  },
+  faucetUrl: "https://faucet.testnet-mud-services.linfra.xyz",
+} as const satisfies MUDChain;
 
 /*
  * See https://mud.dev/tutorials/minimal/deploy#run-the-user-interface
  * for instructions on how to add networks.
  */
-export const supportedChains: MUDChain[] = [mudFoundry, latticeTestnet]
+export const supportedChains: MUDChain[] = [mudFoundry, latticeTestnet];

ce4418f30f1d09e85ddadc3caccb91aa1171cdfb
jackson <jaxernst@gmail.com>
2024-02-02 22:57:52 -0800

    change drip balance

--- packages/client/src/lib/mud/setupNetwork.ts
+++ packages/client/src/lib/mud/setupNetwork.ts
@@ -103,7 +103,7 @@ export async function setupNetwork(userWallet: Wallet) {
     const requestDrip = async () => {
       const balance = await publicClient.getBalance({ address });
       console.info(`[Dev Faucet]: Player balance -> ${balance}`);
-      const lowBalance = balance < parseEther("1");
+      const lowBalance = balance < parseEther(".2");
       if (lowBalance) {
         console.info("[Dev Faucet]: Balance is low, dripping funds to player");
         // Double drip

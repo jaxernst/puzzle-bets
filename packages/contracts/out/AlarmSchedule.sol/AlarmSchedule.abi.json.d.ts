be5320cc22ab58c1b1463c5e9abca0c2e0d3f301
jaxernst <51183683+jaxernst@users.noreply.github.com>
2024-02-07 17:41:23 -0800

    Build contracts

---
+++ packages/contracts/out/AlarmSchedule.sol/AlarmSchedule.abi.json.d.ts
@@ -0,0 +1,15 @@
+declare const abi: [
+  {
+    "type": "event",
+    "name": "ScheduleInitialized",
+    "inputs": [
+      {
+        "name": "alarmTime",
+        "type": "uint256",
+        "indexed": false,
+        "internalType": "uint256"
+      }
+    ],
+    "anonymous": false
+  }
+]; export default abi;
---
+++ packages/contracts/out/IPuzzleGameSystem.sol/IPuzzleGameSystem.abi.json.d.ts
@@ -0,0 +1,77 @@
+declare const abi: [
+  {
+    "type": "function",
+    "name": "games__claim",
+    "inputs": [
+      {
+        "name": "gameId",
+        "type": "bytes32",
+        "internalType": "bytes32"
+      }
+    ],
+    "outputs": [],
+    "stateMutability": "nonpayable"
+  },
+  {
+    "type": "function",
+    "name": "games__joinGame",
+    "inputs": [
+      {
+        "name": "gameId",
+        "type": "bytes32",
+        "internalType": "bytes32"
+      }
+    ],
+    "outputs": [],
+    "stateMutability": "payable"
+  },
+  {
+    "type": "function",
+    "name": "games__newGame",
+    "inputs": [
+      {
+        "name": "gameType",
+        "type": "uint8",
+        "internalType": "enum Game"
+      },
+      {
+        "name": "submissionWindowSeconds",
+        "type": "uint32",
+        "internalType": "uint32"
+      },
+      {
+        "name": "inviteExpirationTimestamp",
+        "type": "uint256",
+        "internalType": "uint256"
+      }
+    ],
+    "outputs": [],
+    "stateMutability": "payable"
+  },
+  {
+    "type": "function",
+    "name": "games__submitSolution",
+    "inputs": [
+      {
+        "name": "gameId",
+        "type": "bytes32",
+        "internalType": "bytes32"
+      }
+    ],
+    "outputs": [],
+    "stateMutability": "nonpayable"
+  },
+  {
+    "type": "function",
+    "name": "games__voteRematch",
+    "inputs": [
+      {
+        "name": "gameId",
+        "type": "bytes32",
+        "internalType": "bytes32"
+      }
+    ],
+    "outputs": [],
+    "stateMutability": "nonpayable"
+  }
+]; export default abi;

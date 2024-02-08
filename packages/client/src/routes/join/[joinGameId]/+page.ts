dc6911b991b20fcc9125f0c81c00aa9c51c4bbf0
jackson <jaxernst@gmail.com>
2024-02-07 22:48:35 -0800

    Tweak meta tags

--- packages/client/src/routes/join/[joinGameId]/+page.ts
+++ packages/client/src/routes/join/[joinGameId]/+page.ts
@@ -2,10 +2,15 @@ import { capitalized } from "$lib/util";
 
 export async function load({ url }) {
   const gameType = capitalized(url?.searchParams.get("gameType") ?? "game");
-  const senderName = url?.searchParams.get("from");
+
+  const senderName =
+    url?.searchParams.get("from")?.split("_").join(" ") ?? null;
+
+  const usdValue = url?.searchParams.get("valUsd");
 
   return {
     gameType,
     senderName,
+    usdValue,
   };
 }

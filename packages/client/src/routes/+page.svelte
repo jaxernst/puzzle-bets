c19d869fe3b372578cfc8eb01dbfeae5864e3c53
jaxernst <51183683+jaxernst@users.noreply.github.com>
2024-02-07 17:29:41 -0800

    Prerender fixes

--- packages/client/src/routes/+page.svelte
+++ packages/client/src/routes/+page.svelte
@@ -1,6 +1,3 @@
-<script>
-</script>
-
 <svelte:head>
   <title>Puzzle Bets | Home</title>
   <meta name="description" content="Solve puzzles with friends" />
--- packages/client/src/routes/+page.ts
+++ packages/client/src/routes/+page.ts
@@ -1,9 +1,6 @@
-// since there's no dynamic data here, we can prerender
-
 import { redirect } from "@sveltejs/kit";
 
-// it so that it gets served as a static asset in production
-export const prerender = true;
+export const prerender = false;
 
 export const load = () => {
   redirect(307, "/welcome");

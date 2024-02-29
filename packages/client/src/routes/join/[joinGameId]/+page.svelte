1c77bb8a88cc17579e54fe8c954162673c95d265
jackson <jaxernst@gmail.com>
2024-02-28 21:25:17 -0800

    Refactor title gen

--- packages/client/src/routes/join/[joinGameId]/+page.svelte
+++ packages/client/src/routes/join/[joinGameId]/+page.svelte
@@ -1,22 +1,7 @@
 <script>
-  import { formatAsDollar } from "$lib/util";
-
   export let data;
 
-  let { gameType, senderName, usdValue } = data;
-  senderName = senderName?.split("_").join(" ") ?? null;
-
-  let inviteMessage =
-    "Accept the challenge and solve the puzzle to win the pot";
-
-  let title = "Puzzle Bets! ";
-  if (senderName) {
-    title += `${senderName} challenged you to a ${
-      usdValue ? formatAsDollar(Number(usdValue)) : ""
-    } ${gameType ?? ""} game`;
-  } else {
-    title += `${gameType} Game Invite`;
-  }
+  let { inviteMessage, title } = data;
 </script>
 
 <svelte:head>
--- packages/client/src/routes/join/[joinGameId]/+page.ts
+++ packages/client/src/routes/join/[joinGameId]/+page.ts
@@ -1,16 +1,46 @@
-import { capitalized } from "$lib/util";
+import { capitalized, formatAsDollar } from "$lib/util";
+
+function getGameType(url: URL) {
+  const gameType = url?.searchParams.get("gameType") ?? "game";
+  return capitalized(gameType);
+}
+
+function getSenderName(url: URL) {
+  const senderNameParam = url?.searchParams.get("from");
+  return senderNameParam ? senderNameParam.split("_").join(" ") : null;
+}
+
+function formatChallengeAmount(usdValue: string) {
+  return usdValue ? `${formatAsDollar(Number(usdValue))} ` : "";
+}
 
 export async function load({ url }) {
-  const gameType = capitalized(url?.searchParams.get("gameType") ?? "game");
+  const gameTypeParam = url?.searchParams.get("gameType");
+  const gameType = gameTypeParam ? capitalized(gameTypeParam) : "";
 
-  const senderName =
-    url?.searchParams.get("from")?.split("_").join(" ") ?? null;
+  const senderNameParam = url?.searchParams.get("from");
+  const senderName = senderNameParam
+    ? senderNameParam.split("_").join(" ")
+    : null;
 
   const usdValue = url?.searchParams.get("valUsd");
 
+  const challengeAmountString = usdValue
+    ? formatAsDollar(Number(usdValue)) + " "
+    : "";
+
+  let title = "Puzzle Bets!";
+  if (senderName) {
+    title += ` ${senderName} challenged you to a ${challengeAmountString}${gameType} game`;
+  } else {
+    title += ` You've been challenged to a ${challengeAmountString}${gameType} game`;
+  }
+
+  const inviteMessage =
+    "Accept the challenge and solve the puzzle to win the pot";
+
   return {
-    gameType,
-    senderName,
-    usdValue,
+    inviteMessage,
+    title,
   };
 }

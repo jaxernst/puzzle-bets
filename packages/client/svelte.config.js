a6168fd4b62390d2f2f5bfd054dff616dfaf9958
jaxernst <51183683+jaxernst@users.noreply.github.com>
2024-02-28 16:48:58 -0800

    Node version

--- packages/client/svelte.config.js
+++ packages/client/svelte.config.js
@@ -7,7 +7,7 @@ const config = {
   // for more information about preprocessors
   preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter(), 
+    adapter: adapter({ runtime: "nodejs20.x"}), 
   } 
 };
 

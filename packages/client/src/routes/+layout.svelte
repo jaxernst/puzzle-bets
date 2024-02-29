7ab0ece41868eedab2d768be2606ac4270b98369
jaxernst <51183683+jaxernst@users.noreply.github.com>
2024-02-28 16:12:01 -0800

    Fix pathname

--- packages/client/src/routes/+layout.svelte
+++ packages/client/src/routes/+layout.svelte
@@ -44,7 +44,7 @@
 
 <div class="w-full h-full fixed">
   <main class="h-full flex flex-col max-w-[36rem] mx-auto text-white">
-    {#if $user || !$page.url.pathname.includes("welcome")}
+    {#if $user || $page.url.pathname !== "/"}
       <section in:slide class="px-3 pt-2">
         <AppHeader />
       </section>
@@ -60,7 +60,7 @@
             goto(`/games/${option.toLowerCase()}/demo`);
           }}
         />
-        {#if $page.url.pathname !== "/welcome"}
+        {#if $page.url.pathname !== "/"}
           <button
             class="text-sm rounded-full px-2 border border-lime-500 text-lime-500 font-semibold disabled:opacity-50 active:bg-neutral-300"
             on:click={() => (showNewGameModal = true)}

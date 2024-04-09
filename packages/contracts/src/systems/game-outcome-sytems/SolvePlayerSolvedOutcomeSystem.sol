f304245ea359b65d7a515a7d05954606f6a8aba0
jackson <jaxernst@gmail.com>
2024-04-08 23:26:35 -0700

    MErge outcome system and bet system

--- packages/contracts/src/systems/game-outcome-sytems/SolvePlayerSolvedOutcomeSystem.sol
+++
@@ -1,7 +0,0 @@
-/**
- * Outcome systems determines who 'wins' and how much of a bet pool should be allocated
- */
-
-contract SolePlayerSolvedOutcomeSystem is System {
-  function getAllocation() public {}
-}

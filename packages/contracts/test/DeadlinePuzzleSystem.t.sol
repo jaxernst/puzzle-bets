dfb811aecc61f7ec26a92e8c48f52376ee5491ee
jackson <jaxernst@gmail.com>
2024-04-10 23:37:17 -0700

    Test cases

--- packages/contracts/test/DeadlinePuzzleSystem.t.sol
+++ packages/contracts/test/DeadlinePuzzleSystem.t.sol
@@ -7,8 +7,8 @@ import { Puzzle, Status } from "../src/codegen/common.sol";
 import { MudTest } from "@latticexyz/world/test/MudTest.t.sol";
 import { PuzzleMasterEoa, RematchCount, Balance, BuyIn, PuzzleType, Player1, Player2, GameStatus, SubmissionWindow, GameStartTime, Solved, InviteExpiration, VoteRematch } from "../src/codegen/index.sol";
 
-contract DeadlinePuzzleSystem is MudTest {
-  function test_game_creation() public {
+contract DeadlinePuzzleSystemTest is MudTest {
+  function test_newGame_GameVariablesAreSetOnCreation() public {
     bytes32 gameId = IWorld(worldAddress).v1__newGame{ value: 1 ether }({
       puzzleType: Puzzle.Wordle,
       submissionWindowSeconds: 1,
@@ -27,13 +27,110 @@ contract DeadlinePuzzleSystem is MudTest {
     assertEq(InviteExpiration.get(gameId), block.timestamp + 100);
   }
 
-  function test_cancel_created_game() public {}
+  function test_joinGame_JoinGameRequiresBuyInDeposit() public {
+    address opponent = address(0x123);
+    bytes32 gameId = IWorld(worldAddress).v1__newGame{ value: 1 ether }({
+      puzzleType: Puzzle.Wordle,
+      submissionWindowSeconds: 1,
+      inviteExpirationTimestamp: block.timestamp + 100,
+      opponent: opponent,
+      puzzleMaster: address(0x456)
+    });
 
-  function test_join_game() public {}
+    vm.startPrank(opponent);
+    vm.deal(opponent, 1 ether);
+    vm.expectRevert("Insufficient buy in");
+    IWorld(worldAddress).v1__joinGame(gameId);
+    assertEq(Balance.get(gameId, opponent), 0);
+
+    IWorld(worldAddress).v1__joinGame{ value: 1 ether }(gameId);
+    assertEq(Balance.get(gameId, opponent), 1 ether);
+  }
+
+  function test_joinGame_OnlySpecifiedOpponentCanJoinGame() public {
+    address opponent = address(0x123);
+    bytes32 gameId = IWorld(worldAddress).v1__newGame({
+      puzzleType: Puzzle.Wordle,
+      submissionWindowSeconds: 1,
+      inviteExpirationTimestamp: block.timestamp + 100,
+      opponent: opponent,
+      puzzleMaster: address(0x456)
+    });
+
+    vm.prank(address(0x999));
+    vm.expectRevert("You are not the specified player2");
+    IWorld(worldAddress).v1__joinGame(gameId);
+
+    vm.prank(opponent);
+    IWorld(worldAddress).v1__joinGame(gameId);
+  }
+
+  function test_joinGame_CancelledGameCannotBeJoined() public {
+    bytes32 gameId = IWorld(worldAddress).v1__newGame{ value: 1 ether }({
+      puzzleType: Puzzle.Wordle,
+      submissionWindowSeconds: 1,
+      inviteExpirationTimestamp: block.timestamp + 100,
+      opponent: address(0x123),
+      puzzleMaster: address(0x456)
+    });
+
+    IWorld(worldAddress).v1__cancelPendingGame(gameId);
+
+    vm.prank(address(0x123));
+    vm.expectRevert();
+    IWorld(worldAddress).v1__joinGame{ value: 1 ether }(gameId);
+  }
+
+  function test_joinGame_GameIsStartedWhenOpponentJoins() public {
+    bytes32 gameId = IWorld(worldAddress).v1__newGame({
+      puzzleType: Puzzle.Wordle,
+      submissionWindowSeconds: 1,
+      inviteExpirationTimestamp: block.timestamp + 100,
+      opponent: address(0),
+      puzzleMaster: address(0x456)
+    });
+
+    vm.prank(address(0x999));
+    IWorld(worldAddress).v1__joinGame(gameId);
+    assertEq(Player2.get(gameId), address(0x999));
+    assertEq(uint(GameStatus.get(gameId)), uint(Status.Active));
+  }
+
+  function test_cancelPendingGame_BuyInReturnedWhenCreatorCancelsGame() public {
+    uint startingBalance = address(this).balance;
+    bytes32 gameId = IWorld(worldAddress).v1__newGame{ value: 1 ether }({
+      puzzleType: Puzzle.Wordle,
+      submissionWindowSeconds: 1,
+      inviteExpirationTimestamp: block.timestamp + 100,
+      opponent: address(0x123),
+      puzzleMaster: address(0x456)
+    });
+
+    assertEq(address(this).balance, startingBalance - 1 ether);
+    IWorld(worldAddress).v1__cancelPendingGame(gameId);
+    assertEq(address(this).balance, startingBalance);
+  }
+
+  function test_cancelPendingGame_GameCannotBeCancelledOnceStarted() public {
+    bytes32 gameId = IWorld(worldAddress).v1__newGame{ value: 1 ether }({
+      puzzleType: Puzzle.Wordle,
+      submissionWindowSeconds: 1,
+      inviteExpirationTimestamp: block.timestamp + 100,
+      opponent: address(0),
+      puzzleMaster: address(0x456)
+    });
+
+    IWorld(worldAddress).v1__joinGame{ value: 1 ether }(gameId);
+
+    vm.expectRevert("Game is not pending");
+    IWorld(worldAddress).v1__cancelPendingGame(gameId);
+  }
 
   function test_submission_solution() public {}
 
   function test_rematching() public {}
 
   function test_claim() public {}
+
+  receive() external payable {}
 }

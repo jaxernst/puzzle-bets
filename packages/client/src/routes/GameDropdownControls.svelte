15286761b0e63cd763e8da0aba884b6ba163f05c
jackson <jaxernst@gmail.com>
2024-04-17 23:12:59 -0700

    Format

--- packages/client/src/routes/GameDropdownControls.svelte
+++ packages/client/src/routes/GameDropdownControls.svelte
@@ -1,23 +1,23 @@
 <script lang="ts">
-  import { page } from "$app/stores";
-  import Dropdown from "$lib/components/Dropdown.svelte";
-  import { goto } from "$app/navigation";
-  import { user } from "$lib/user";
-  import { intToEntity } from "$lib/util";
-  import { type PuzzleType } from "$lib/types";
-  import { SUPPORTED_GAME_TYPES } from "$lib/constants";
-  import { clickOutside } from "$lib/actions/clickOutside";
-  import Modal from "$lib/components/Modal.svelte";
-  import NewGame from "./games/NewGame.svelte";
-  import { slide } from "svelte/transition";
+  import { page } from "$app/stores"
+  import Dropdown from "$lib/components/Dropdown.svelte"
+  import { goto } from "$app/navigation"
+  import { user } from "$lib/user"
+  import { intToEntity } from "$lib/util"
+  import { type PuzzleType } from "$lib/types"
+  import { SUPPORTED_GAME_TYPES } from "$lib/constants"
+  import { clickOutside } from "$lib/actions/clickOutside"
+  import Modal from "$lib/components/Modal.svelte"
+  import NewGame from "./games/NewGame.svelte"
+  import { slide } from "svelte/transition"
 
   const gameNames = ["Wordle", "Connections", "Crossword", "Sudoku"]
   $: gameRoute = gameNames.find((game) =>
     $page.url.pathname.includes("games/" + game.toLowerCase()),
   )
 
-  $: dropdownSelection = gameRoute ?? null;
-  $: gameType = (dropdownSelection?.toLowerCase() ?? "wordle") as PuzzleType;
+  $: dropdownSelection = gameRoute ?? null
+  $: gameType = (dropdownSelection?.toLowerCase() ?? "wordle") as PuzzleType
 
   const homeRoutes = ["/", "/welcome", "/about"]
 

d92ff711e86bcb7412550c23f8da457476727e47
jackson <jaxernst@gmail.com>
2024-04-21 16:57:48 -0700

    primary button style

---
+++ packages/client/src/lib/components/ButtonPrimary.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { twMerge } from "tailwind-merge"
+
+  let overrideClass = ""
+
+  export { overrideClass as class }
+</script>
+
+<button
+  on:click
+  class={twMerge(
+    "rounded-lg bg-lime-500 px-2 py-1 transition-all hover:bg-lime-400 hover:shadow-lg active:bg-lime-600",
+    overrideClass,
+  )}
+>
+  <slot />
+</button>

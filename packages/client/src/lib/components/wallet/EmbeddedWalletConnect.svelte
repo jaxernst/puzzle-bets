daba9c1efc9809b69429a427920fb3699d294c51
jackson <jaxernst@gmail.com>
2024-05-15 21:13:48 -0700

    Email flow working

--- packages/client/src/lib/components/wallet/EmbeddedWalletConnect.svelte
+++ packages/client/src/lib/components/wallet/EmbeddedWalletConnect.svelte
@@ -1,11 +1,18 @@
+<script context="module" lang="ts">
+  import { writable } from "svelte/store"
+  const showConfirmCode = writable(false)
+  const emailInput = writable("")
+  const emailError = writable<string | null>(null)
+</script>
+
 <script lang="ts">
-  import { browser } from "$app/environment"
   import Apple from "$lib/icons/Apple.svelte"
   import Google from "$lib/icons/Google.svelte"
   import { tw, walletStore } from "$lib/mud/connectWallet"
   import AnimatedArrow from "../AnimatedArrow.svelte"
-  import { preAuthenticate } from "thirdweb/wallets/embedded"
   import ConfirmationCodeInput from "./ConfirmationCodeInput.svelte"
+  import ButtonPrimary from "../ButtonPrimary.svelte"
+  import { preAuthenticate } from "thirdweb/wallets/embedded"
 
   export let onConnect: () => void = () => {}
 
@@ -14,35 +21,51 @@
     if (wallet) onConnect()
   }
 
-  let emailInput: string
   async function startEmailVerification() {
-    console.log("sending email to", emailInput)
-    await preAuthenticate({
-      client: tw,
-      strategy: "email",
-      email: emailInput,
-    })
+    console.log("sending email to", $emailInput)
+    if (!$emailInput.length) {
+      $emailError = "Please enter a valid email"
+      return
+    }
 
-    showConfirmCode = true
+    $emailError = null
+
+    try {
+      await preAuthenticate({
+        client: tw,
+        strategy: "email",
+        email: $emailInput,
+      })
+    } catch (e) {
+      console.log(e)
+
+      if (JSON.stringify(e?.toString?.() || "{}").includes("Invalid email")) {
+        $emailError = "Email not valid"
+      } else {
+        $emailError = "An unknown error occured. Please try again later"
+      }
+      return
+    }
+
+    showConfirmCode.set(true)
   }
 
-  let codeError = null
   async function connectWithEmailCode(code: string) {
-    codeError = null
+    $emailError = null
 
     try {
       await walletStore.tryConnect("email", {
-        email: emailInput,
+        email: $emailInput,
         verificationCode: code,
       })
 
       onConnect()
     } catch (e) {
-      console.log(e)
+      console.log("set email error")
+      $emailError = "Verification code not valid"
+      return
     }
   }
-
-  let showConfirmCode = false
 </script>
 
 <div class="flex flex-col items-center gap-4">
@@ -72,40 +95,44 @@
     <div class="w-1/3 border-t-[.5px] border-neutral-400"></div>
   </div>
 
-  {#if showConfirmCode && emailInput}
-    <div class="flex flex-col gap-1">
+  {#if $showConfirmCode && $emailInput}
+    <div class="flex flex-col gap-2">
       <div class="text-center text-sm text-neutral-400">
         Enter the verification code sent to
-        <div class="text-neutral-200">{emailInput}</div>
+        <div class="text-neutral-200">{$emailInput}</div>
       </div>
-      <div>
-        <ConfirmationCodeInput length="6" onComplete={connectWithEmailCode} />
-      </div>
+      <ConfirmationCodeInput length="6" onComplete={connectWithEmailCode} />
     </div>
   {:else}
-    <div
+    <form
       class="mx-5 flex items-center gap-2 rounded-lg border border-neutral-500"
+      on:submit={startEmailVerification}
     >
       <div class="flex-grow">
         <input
           type="text"
           class="w-full border-none bg-transparent px-4 py-2 outline-none"
           placeholder="Enter email address"
-          bind:value={emailInput}
+          bind:value={$emailInput}
         />
       </div>
 
       <div class="translate-y-[.065rem] px-1.5">
-        <button
-          on:click={startEmailVerification}
+        <ButtonPrimary
           class="rounded-md border-lime-500 bg-lime-500 px-1 py-1 text-center font-semibold text-white"
         >
           <AnimatedArrow
             direction="right"
             class="h-5 w-5 fill-white stroke-white"
           />
-        </button>
+        </ButtonPrimary>
       </div>
+    </form>
+  {/if}
+
+  {#if $emailError}
+    <div class="text-sm text-red-600">
+      {$emailError}
     </div>
   {/if}
 </div>

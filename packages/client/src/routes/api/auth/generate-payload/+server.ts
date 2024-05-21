f9c8abb8e0d56c1daad0a241c2c26c7662df638a
jackson <jaxernst@gmail.com>
2024-05-20 21:46:09 -0700

    Move stuff around

--- packages/client/src/routes/api/auth/generate-payload/+server.ts
+++ packages/client/src/routes/api/auth/generate-payload/+server.ts
@@ -1,6 +1,4 @@
-import { twAuth } from "$lib/server/twAuth"
 import type { RequestHandler } from "./$types"
-import { generateNonce } from "siwe"
 
 export const GET: RequestHandler = async ({ request }) => {
   const { address } = await request.json()

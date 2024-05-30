72c95fc776ec0dcb260c3a5c4925f896dab29b42
jackson <jaxernst@gmail.com>
2024-05-29 21:39:48 -0700

    Delete unused

--- packages/client/src/routes/api/siwer/nonce/+server.ts
+++
@@ -1,8 +0,0 @@
-import { twAuth } from "$lib/thirdweb"
-import type { RequestHandler } from "../../siwer/nonce/$types"
-
-export const POST: RequestHandler = async ({ request }) => {
-  const { address } = await request.json()
-  const loginPayload = await twAuth.generatePayload({ address })
-  return new Response(JSON.stringify(loginPayload), { status: 200 })
-}
